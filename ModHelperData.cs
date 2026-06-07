1b5a3f967f41974dee05174a424140827533c81b
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-06-06 19:39:29 -0700

    Fix typo in mod description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "ArtifactAndPerkEnabler";
 
-    public const string Description = "Allows you to turn on perks from social seasons and artifacts from rogue legends in any game mode. Ther mod only work if you are flagged to keep unflagged people from messing with social seasons. The artifacts also only work if you have purchased the DLC as otherwise you could get most of the crazy interactions without purchasing it.";
+    public const string Description = "Allows you to turn on perks from social seasons and artifacts from rogue legends in any game mode. The mod only work if you are flagged to keep unflagged people from messing with social seasons. The artifacts also only work if you have purchased the DLC as otherwise you could get most of the crazy interactions without purchasing it.";
 
     public const string RepoOwner = "RedBlooN-YT";
     public const string RepoName = "ArtifactAndPerkEnabler";
